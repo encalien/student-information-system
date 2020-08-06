7d146e2410f0fa9f5749d29eb606974158cdf5cf
Katarina <katarina.mursec@gmail.com>
2020-08-06 09:39:40 +0200

    add professors overview

--- src/app/components/professors/professors.component.ts
+++ src/app/components/professors/professors.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/api'; 
+import { Router } from '@angular/router';
+import { Professor } from '../../models/professor';
+// import { ProfessorsService } from '../../services/professors.service';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-professors',
@@ -7,10 +12,72 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ProfessorsComponent implements OnInit {
+  allProfessors: Professor[];
+  listedProfessors: Professor[];
+  totalRecords: number;
+  columns: any[];
+  loading: boolean;
 
-  constructor() { }
+  selectedProfessor: Professor;
+  
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router) {
+  }
+
 
   ngOnInit(): void {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['login']);
+    }
+
+    this.getProfessors();
+    this.defineColumns();
+    this.loading = true;
   }
+
+  getProfessors(): void {
+    this.allProfessors = [
+      {
+        "id": "1",
+        "firstName": "Albus",
+        "lastName": "Dumbledore",
+        "taughtCourseIds": [
+          1
+        ]
+      },
+      {
+        "id": "1",
+        "firstName": "Minerva",
+        "lastName": "McGonagal",
+        "taughtCourseIds": [
+          2
+        ]
+      }
+    ];
+    this.totalRecords = this.allProfessors.length;
+  }
+
+  defineColumns(): void {
+    this.columns = [
+      { field: 'firstName', header: 'First Name' },
+      { field: 'lastName', header: 'Last Name' },
+      { field: 'taughtCourseIds', header: 'Teaching Courses' },
+    ];
+  }
+
+  lazyLoadProfessors(event: LazyLoadEvent): void {
+    this.loading = true;
+
+    //imitate db connection over a network
+    setTimeout(() => {
+      if (this.allProfessors) {
+          this.listedProfessors = this.allProfessors.slice(event.first, (event.first + event.rows));
+          this.loading = false;
+      }
+    }, 1000);  
+  }
+
+
 
 }
--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -56,13 +56,6 @@ export class StudentsComponent implements OnInit {
   lazyLoadStudents(event: LazyLoadEvent): void {
     this.loading = true;
 
-    //in a real application, make a remote request to load data using state metadata from event
-    //event.first = First row offset
-    //event.rows = Number of rows per page
-    //event.sortField = Field name to sort with
-    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
-
     //imitate db connection over a network
     setTimeout(() => {
       if (this.allStudents) {
